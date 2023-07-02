a6d1ce48f1a7a54dea35e370313fced06c62e8f4
Lakshan2000610 <mhplakshan98@gmail.com>
2023-07-02 19:23:44 +0530

    Exercise 1
    Using C program
    
    01.Print a welcome message
    02.Print an F letter using " # "
    03.Print a C letter using " # "

---
+++ ICT20879.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+   // my Details
+
+   printf("Hello, Welcome!\n");
+   printf("\n");
+   printf("My Personnel Details\n");
+   printf("----------------------------\n");
+   printf("Name: M.H.P.Lakshan \n");
+   printf("Home Town:Haldummulla\n");
+   printf("Year: First Year\n");
+
+   printf("\n \n");
+
+
+// Write a C program to print an F letter using following pattern.
+
+    printf("#####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("####\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#");
+
+    printf("\n\n");
+
+
+// Write a C program to print a C letter using following pattern
+
+    printf("\    #####\n");
+    printf(" ##");
+    printf("\        ##\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf("#\n");
+    printf(" ##");
+    printf("\        ##\n");
+    printf("\    #####\n");
+
+
+
+    return 0;
+}
